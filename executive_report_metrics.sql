4161ed23d8cc51d85ca62009555ca16ab7cf2571
joyce92200 <jowork2024@gmail.com>
2025-11-29 12:56:24 +0100

    Update executive_report_metrics.sql

--- executive_report_metrics.sql
+++ executive_report_metrics.sql
@@ -1,17 +1,32 @@
+-- ========================================
+-- Revenue Metrics
+-- ========================================
 -- revenue per customer
 -- revenue per week
 -- profit by eatery
 -- profit per month
+
+-- ========================================
+-- User Metrics
+-- ========================================
 -- registrations by month
--- monthly active users(MAU)
+-- monthly active users (MAU)
 -- registrations running total
--- MAU monitor I
--- MAU monitor II
--- MAU monitor III
--- Order growth rate
+-- MAU monitor I, II, III
+
+-- ========================================
+-- Growth Metrics
+-- ========================================
+-- order growth rate
 -- retention rate
--- Average Revenue Per User(ARPU) by month
--- APRU by user ID
--- APRU per week to see whether APRU has increased over time
--- average orders per user
--- bucketing using CASE expression : customized histograms to visualize the frequencies of each value in a dataset
+
+-- ========================================
+-- ARPU Metrics
+-- ========================================
+-- Average Revenue Per User (ARPU) by month
+-- ARPU by user ID
+-- ARPU per week to see trends
+
+-- ========================================
+-- Other Metrics
+-- ========================================
